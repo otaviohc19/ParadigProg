9ff73e0b852e00f02b83842320573b234dbfb1b3
Otávio Henrique Cardoso Rodrigues <64485490+otaviohc19@users.noreply.github.com>
2023-04-12 21:47:18 -0300

    atualização

--- scripts.js
+++ scripts.js
@@ -1,31 +1,31 @@
-let btn = document.getElementById("btn")
-btn.addEventListener("click", () => carregaPokemon())
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => carregaPokemon());
 
 function verificaPokemon() {
-    let pokemon = document.getElementById("nome").value
+    let pokemon = document.getElementById("nome").value;
 
-    let url = `https://pokemon.danielpimentel.com.br/v1/pokemon/nome/${pokemon}`
+    let url = `https://pokemon.danielpimentel.com.br/v1/pokemon/nome/${pokemon}`;
 
     return new Promise((resolve, reject) => {
         fetch(url)
         .then((resposta) => resposta.json() )
         .then((pokemon) => resolve(pokemon.pokemon))
-        .catch((erro) => reject(erro))
-    })
+        .catch((erro) => reject(erro));
+    });
 }
 
 function carregaPokemon() {
-    let ger = document.getElementById("geracao").value
+    let ger = document.getElementById("geracao").value;
 
     verificaPokemon().then((pokemon) =>{
         if (ger == pokemon.geracao) {
 
-            let div = document.getElementById("pokedex")
-            div.innerHTML = ""
+            let div = document.getElementById("pokedex");
+            div.innerHTML = "";
 
             let html = `
             <div id="pokedex" class="pokedex">
-                <img src="${pokemon.img}" heigth="500px" width="500px" />
+                <img src="${pokemon.img}" height="500px" width="500px" />
                 <br>
                 Número: ${pokemon.numero}
                 <br>
@@ -37,7 +37,11 @@ function carregaPokemon() {
             div.insertAdjacentHTML("beforeend", html);
 
         } else{
-            alert("O pokemon não é dessa geração")
+            alert("O pokemon não é dessa geração");
         }
-    })
+    }).catch((erro) => {
+        console.error(erro);
+        let div = document.getElementById("pokedex");
+        div.innerHTML = `<p>Erro ao carregar Pokémon. Por favor, verifique o nome e tente novamente.</p>`;
+    });
 }
